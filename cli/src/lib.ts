21a8f4107916d5a8068c44e498ed2a82bc2854c9
Seweryn Kras <seweryn.kras@golem.network>
2025-07-03 10:40:28 +0200

    export cli internals as a library

---
+++ cli/src/lib.ts
@@ -0,0 +1,9 @@
+// TODO: Extract the internals of the CLI into a library
+
+export * from "./scheduler";
+export * from "./app_context";
+export * from "./budget";
+export * from "./instrumentation";
+export * from "./prefix";
+export * from "./results";
+export * from "./node_manager/golem_session";
