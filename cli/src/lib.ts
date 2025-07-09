70cfdcac1ed8cb41eb8f1fe43dfaed90e3c08a7b
wojciech12 <wb@unoperate.com>
2025-07-09 22:16:16 +0200

    Fix lib.ts

--- cli/src/lib.ts
+++ cli/src/lib.ts
@@ -4,7 +4,7 @@ export * from "./scheduler";
 export * from "./app_context";
 export * from "./budget";
 // export * from "./instrumentation"; //TODO: Remove auto-initialization of OpenTelemetry on import
-export * from "./prefix";
+export * from "./params";
 export * from "./results_service";
 export * from "./estimator_service";
 export * from "./node_manager/golem_session";
